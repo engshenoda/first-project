12a825a1fb3d83540d989833332b036e85625170
shenoa <shenoa@gmail.com>
2024-03-29 23:43:07 +0200

    edit in main

--- lib/main2.dart
+++ lib/main2.dart
@@ -31,7 +31,7 @@ class HelloWorldApp extends StatelessWidget {
             height: 100,
             width: 100,
             color: Colors.green,
-            child: const Center(child: Text("hello world")),
+            child: const Center(child: Text(" Hello world")),
           ),
         ],
       ),
